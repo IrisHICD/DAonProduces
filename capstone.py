5c2becf36be3e5527d973233b19e74118fd0e60a
Iris Cheang-Deis <131198414+IrisHICD@users.noreply.github.com>
2024-11-26 12:04:56 -0700

    add comments

--- capstone.py
+++ capstone.py
@@ -1,11 +1,19 @@
-import pandas as pd
-data = pd.read_csv("32100077.csv")
+import pandas as pd # type: ignore
 
+data = pd.read_csv("32100077.csv")
+# Quick look at the data and decided to drop a couple of useless columns
 data.drop(columns=["DECIMALS", "TERMINATED", "DGUID"], inplace=True)
 data.drop(columns=["STATUS", "SYMBOL", "VECTOR"], inplace=True)
-
 data.info()
 
+# Decided to focus on one grain at a time
 barley_data = data[data["Farm products"].str.contains(r'\b\w*Barley\w*\b', regex=True)]
 barley_data.info()
 barley_counts = barley_data["Farm products"].value_counts()
+print(barley_counts)
+# At this point I noticed there are three kind of barley
+
+barley_counts.head()
+
+
+
