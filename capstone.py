c4f6de9f4805ce182102847fb816e1dd58674c3e
Iris Cheang-Deis <131198414+IrisHICD@users.noreply.github.com>
2024-11-27 12:07:15 -0700

    investigate AB data

--- capstone.py
+++ capstone.py
@@ -1,19 +1,29 @@
-import pandas as pd # type: ignore
+import pandas as pd
+# from bokeh.plotting import figure, show, output_notebook
+# from bokeh.models import ColumnDataSource
+# output_notebook()
 
 data = pd.read_csv("32100077.csv")
 # Quick look at the data and decided to drop a couple of useless columns
 data.drop(columns=["DECIMALS", "TERMINATED", "DGUID"], inplace=True)
 data.drop(columns=["STATUS", "SYMBOL", "VECTOR"], inplace=True)
-data.info()
 
+# filtered code
+filtered_data = data[data["Farm_products"].isin([
+    "Barley [1151141]",
+    "Barley for animal feed [115114111]"
+])]
+filtered_data.info()
+
+# Investigation steps
 # Decided to focus on one grain at a time
 barley_data = data[data["Farm products"].str.contains(r'\b\w*Barley\w*\b', regex=True)]
 barley_data.info()
 barley_counts = barley_data["Farm products"].value_counts()
 print(barley_counts)
-# At this point I noticed there are three kind of barley
-
-barley_counts.head()
-
-
+# At this point I noticed there are three kind of barley, wrote filtered code
+# I also want to selected out the only AB from GEO, need to make sure there are entries for AB
+GEO_data = filtered_data["GEO"].value_counts()
+print(GEO_data)
+# it showed 630 entries for AB, great
 
